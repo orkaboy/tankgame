4e357578d8578d6ef17b948569adacc1a3e47eee
Orkaboy <orkaboy@gmail.com>
2022-03-17 23:44:22 +0100

    modernizing

--- mdfgame/world.cpp
+++ mdfgame/world.cpp
@@ -21,35 +21,35 @@ World World_GetWorld(std::string id)
 World World_Init(std::string findpath)
 {
 	std::ifstream pfile;
-	float tempx, tempy, tempr, tempm;
-	std::string tempid;
-	std::string dummy;
-	unsigned int num_planets;
-	
-	World world;
-	
+
 	pfile.open(findpath.c_str(), std::ios::in);
 	if(!pfile)
 	{
 		fmt::print("Couldent find Worlddefine file {}\n", findpath);
 	}
-	
+
+	std::string dummy;
+	unsigned int num_planets;
 	pfile >> dummy >> num_planets;
-	
+
+	World world;
 	for(unsigned int i = 0; i < num_planets; i++)
 	{
+		std::string tempid;
+		float tempx, tempy, tempr, tempm;
+
 		pfile >> dummy >> tempid;
 		pfile >> dummy >> tempx >> tempy;
 		pfile >> dummy >> tempr;
 		pfile >> dummy >> tempm;
-		
+
 		Planet *planet = Planet_Init(tempx, tempy, tempr, tempm);
-		
+
 		Planet_SetImage(planet, tempid);
-		
+
         world.planets.push_back(planet);
 	}
-	
+
 	/** TEMP **/
 	world.size = vec2(2000, 2000);
 
@@ -61,16 +61,15 @@ World World_Init(std::string findpath)
 void World_SpawnAmmo(World &world, float dt)
 {
 	world.timeSinceLastAmmo += dt;
-	
+
 	if (world.timeSinceLastAmmo >= 10.0 ) {
-		
 		vec2 pos;
-		
+
 		pos.x = rand()%(int)world.size.x;
 		pos.y = rand()%(int)world.size.y;
-		
+
 		ProjectileType type;
-		
+
 		switch (rand()%3)
 		{
 			case 0: type = AMMO_TURRET; break;
@@ -78,9 +77,9 @@ void World_SpawnAmmo(World &world, float dt)
 			case 2: type = AMMO_CLUSTER; break;
 			case 3: type = AMMO_ROCKET; break;
 		}
-		
+
 		Projectile* p = Projectile_Create(NULL, 0.0, pos, 0.0, type);
-		
+
         world.projectiles.push_back(p);
 		world.timeSinceLastAmmo = 0.0;
 	}
@@ -88,13 +87,13 @@ void World_SpawnAmmo(World &world, float dt)
 
 void World_DeInit(World &world)
 {
-	for(unsigned int i = 0; i < world.planets.size(); i++)
-		Planet_Free(world.planets[i]);
-	for(unsigned int i = 0; i < world.tanks.size(); i++)
-		Tank_Free(world.tanks[i]);
+	for(auto planet : world.planets)
+		Planet_Free(planet);
+	for(auto tank : world.tanks)
+		Tank_Free(tank);
 
-	for(unsigned int i = 0; i < world.projectiles.size(); i++)
-		Projectile_Destroy(world.projectiles[i]);
-	for(unsigned int i = 0; i < world.effects.size(); i++)
-		Effect_Delete(world.effects[i]);
+	for(auto proj : world.projectiles)
+		Projectile_Destroy(proj);
+	for(auto effect : world.effects)
+		Effect_Delete(effect);
 }
